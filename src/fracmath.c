4ec64ad3ea54cac19469c823019e13c430a48708
OldWorldOrdr <joey.t.reinhart@gmail.com>
2024-02-06 14:17:50 -0500

    upadte

--- src/fracmath.c
+++ src/fracmath.c
@@ -59,36 +59,7 @@ bool isvalidnum(const char *str) {
   return true;
 }
 
-int main(int argc, char *argv[]) {
-  if (argc < 3) {
-    printf("Usage: %s <fraction 1> <fraction 2>\n", argv[0]);
-    return EXIT_FAILURE;
-  }
-  char arg1[strlen(argv[1]) + 1];
-  char arg2[strlen(argv[2]) + 1];
-  strcpy(arg1, argv[1]);
-  strcpy(arg2, argv[2]);
-  char *ptr1 = strchr(arg1, '/');
-  char *ptr2 = strchr(arg2, '/');
-  if (ptr1 == NULL || ptr2 == NULL) {
-    printf("Invalid fraction format\n");
-    return EXIT_FAILURE;
-  }
-  *ptr1 = '\0';
-  *ptr2 = '\0';
-  if (!isvalidnum(arg1) || !isvalidnum(ptr1 + 1)) {
-    printf("Invalid fraction: %s\n", argv[1]);
-    return EXIT_FAILURE;
-  }
-  if (!isvalidnum(arg2) || !isvalidnum(ptr2 + 1)) {
-    printf("Invalid fraction: %s\n", argv[2]);
-    return EXIT_FAILURE;
-  }
-
-  long long num1 = atoll(arg1);
-  long long den1 = atoll(ptr1 + 1);
-  long long num2 = atoll(arg2);
-  long long den2 = atoll(ptr2 + 1);
+void fracadd(long long num1, long long den1, long long num2, long long den2) {
   long long num = num1 * den2 + num2 * den1;
   long long den = den1 * den2;
   long long gcd = 1;
@@ -100,10 +71,69 @@ int main(int argc, char *argv[]) {
   if (num < den)
     printf("%lld/%lld\n", num, den);
   else {
-    printf("Improper fraction: %lld/%lld\n", num, den);
+    long long oldnum = num;
+    long long oldden = den;
     long long whole = num / den;
     num %= den;
-    printf("%lld %lld/%lld\n", whole, num, den);
+    if (num)
+      printf("%lld %lld/%lld\n", whole, num, den);
+    else
+      printf("%lld\n", whole);
+    printf("Improper fraction: %lld/%lld\n", oldnum, oldden);
+  }
+}
+
+int main(int argc, char *argv[]) {
+  if (argc < 4) {
+    printf("Usage: %s <fraction 1> <operation> <fraction 2>\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+  char arg1[strlen(argv[1]) + 1];
+  char arg2[strlen(argv[3]) + 1];
+  strcpy(arg1, argv[1]);
+  strcpy(arg2, argv[3]);
+  char *ptr1 = strchr(arg1, '/');
+  char *ptr2 = strchr(arg2, '/');
+  if (ptr1 == NULL) {
+    printf("Invalid fraction: %s\n", argv[1]);
+    return EXIT_FAILURE;
+  }
+  if (ptr2 == NULL) {
+    printf("Invalid fraction: %s\n", argv[3]);
+    return EXIT_FAILURE;
+  }
+  *ptr1 = '\0';
+  *ptr2 = '\0';
+  if (!isvalidnum(arg1) || !isvalidnum(ptr1 + 1)) {
+    printf("Invalid fraction: %s\n", argv[1]);
+    return EXIT_FAILURE;
+  }
+  if (!isvalidnum(arg2) || !isvalidnum(ptr2 + 1)) {
+    printf("Invalid fraction: %s\n", argv[3]);
+    return EXIT_FAILURE;
+  }
+
+  long long num1 = atoll(arg1);
+  long long den1 = atoll(ptr1 + 1);
+  long long num2 = atoll(arg2);
+  long long den2 = atoll(ptr2 + 1);
+  char operation = argv[2][0];
+  switch (operation) {
+  case '+':
+    fracadd(num1, den1, num2, den2);
+    break;
+  case '-':
+    // fracsub(num1, den1, num2, den2);
+    // break;
+  case '*':
+    // fracmul(num1, den1, num2, den2);
+    // break;
+  case '/':
+    // fracdiv(num1, den1, num2, den2);
+    // break;
+  default:
+    printf("Invalid operation: %c\n", operation);
+    return EXIT_FAILURE;
   }
   return EXIT_SUCCESS;
 }
